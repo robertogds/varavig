19cb8ad37d5f4bb111c1990eea27f20dba37247a
robertogds <roberto@imaginasoft.com>
2011-02-17 23:48:21 +0100

    big refactor fase 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Varavig::Application.routes.draw do
   resources :todos
 
-  get "home/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +50,7 @@ Varavig::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  #root :to => "home#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
