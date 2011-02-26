16153778e163ffc36e8e173389444087f3055194
Carlo Scarioni <carlo.scarioni@gmail.com>
2011-02-26 10:15:59 +0000

    Addded empty task.js starting

---
+++ public/javascripts/models/task.js
@@ -0,0 +1,12 @@
+/**
+ * Created by .
+ * User: cscarioni
+ * Date: 24-Feb-2011
+ * Time: 22:40:42
+ * To change this template use File | Settings | File Templates.
+ */
+
+var Task = Backbone.Model.extend({
+
+
+});
