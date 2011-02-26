219db9391c233c493a7958232797d4af5d338cd0
Carlo <carlo.scarioni@gmail.com>
2011-02-26 05:00:23 -0800

    Story table name, no stories

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(:version => 20110209193957) do
 
-  create_table "stories", :force => true do |t|
+  create_table "story", :force => true do |t|
     t.text     "content"
     t.integer  "order"
     t.boolean  "done"
